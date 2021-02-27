feb8ffb60799f1fcfcb3f0c11658f91714ece30f
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-02-27 13:07:33 -0300

    wip

---
+++ src/containers/Restricted/index.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import styles from './Restricted.module.scss';
+
+const NotFound: React.FC = () => {
+  return (
+    <div className={styles.container}>
+      <p>Restricted</p>
+    </div>
+  );
+};
+
+export default NotFound;
--- src/routes.tsx
+++ src/routes.tsx
@@ -5,16 +5,18 @@ import NotFound from './containers/NotFound';
 import Unauthorized from './containers/Unauthorized';
 import AuthRoute from './utils/AuthRoute';
 import NoAuthRoute from './utils/NoAuthRoute';
+import Restricted from './containers/Restricted';
 import ScrollToTop from './utils/ScrollToTop';
 
 export enum PublicRoutes {
   login = '/login',
-  notFound = 'not-found',
-  unauthorized = 'unauthorized',
+  notFound = '/not-found',
+  unauthorized = '/unauthorized',
 }
 
 export enum AuthRoutes {
   home = '/',
+  restricted = '/restricted',
 }
 
 const routes = (
@@ -22,12 +24,19 @@ const routes = (
     <ScrollToTop />
     <Switch>
       <AuthRoute exact path={AuthRoutes.home} component={Home} />
+      <AuthRoute
+        exact
+        path={AuthRoutes.restricted}
+        component={Restricted}
+        requiredRoles={['admin']}
+      />
 
-      <Route exact path={PublicRoutes.login} component={Login} />
+      <Route path={PublicRoutes.login} component={Login} />
       <Route path={PublicRoutes.unauthorized} component={Unauthorized} />
       <Route path={PublicRoutes.notFound} component={NotFound} />
 
       <NoAuthRoute />
+      <Route component={NotFound} />
     </Switch>
   </>
 );
