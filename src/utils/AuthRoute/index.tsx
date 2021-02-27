740a869d817bdb952d75ffc5548047784d75a927
gabrielsanttana <gabriel.gsantana7@gmail.com>
2021-02-27 11:17:49 -0300

    Add AuthRoute higher order component

---
+++ src/utils/AuthRoute/index.tsx
@@ -0,0 +1,71 @@
+import React from 'react';
+import {connect} from 'react-redux';
+import {Route, RouteComponentProps} from 'react-router';
+import {Redirect} from 'react-router-dom';
+import {PublicRoutes} from '../../routes';
+import {ApplicationState} from '../../store/reducers/rootReducer';
+
+export type UserRoles = 'admin' | 'staff' | 'nonAdmin';
+
+interface StateProps {
+  isAuthed?: boolean;
+  isStaff?: boolean;
+  isAdmin?: boolean;
+  location?: any;
+}
+
+interface OwnProps {
+  Component: React.FC<any>;
+  path: string;
+  exact?: boolean;
+  requiredRoles?: UserRoles[];
+}
+
+type AuthRouteProps = OwnProps & StateProps;
+
+const AuthRoute: React.FC<AuthRouteProps> = ({
+  Component,
+  path,
+  exact = false,
+  requiredRoles,
+  isAuthed,
+  isStaff,
+  isAdmin,
+}) => {
+  const userRole = isAdmin ? 'admin' : isStaff ? 'staff' : 'nonAdmin';
+  const userHasRequiredRole = requiredRoles?.includes(userRole) ?? true;
+  const allowAccess = isAuthed && userHasRequiredRole;
+
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={(props: RouteComponentProps) => {
+        if (!isAuthed)
+          return (
+            <Redirect
+              to={{
+                pathname: PublicRoutes.login,
+              }}
+            />
+          );
+
+        if (allowAccess) return <Component {...props} />;
+
+        return (
+          <Redirect
+            to={{
+              pathname: PublicRoutes.unauthorized,
+            }}
+          />
+        );
+      }}
+    />
+  );
+};
+
+export default connect((state: ApplicationState) => ({
+  isAuthed: state.getIn(['authentication', 'data', 'is_authenticated']),
+  isStaff: state.getIn(['authentication', 'data', 'user', 'is_staff']),
+  isAdmin: state.getIn(['authentication', 'data', 'user', 'is_superuser']),
+}))(AuthRoute);
